20d4d148d025723cb011a894f414c427adbe44b8
Devon Muraoka <geekmann2@gmail.com>
2018-07-07 15:51:12 -0700

    Add error handling to overcoach

--- overcoach.sh
+++ overcoach.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-set -e
+
+function error_handler {
+	original_message=`git log -1 --pretty=%B`
+	error_message=`cat error.log`
+	full_message=`echo $original_message \n\n $error_message`
+	git commit --amend -m "$full_message"
+	
+	echo $error_message
+	exit 1
+}
+
 git add .
 git commit -m "$*"
 rm -f images/*
 #docker build -t overcoach .
-docker run -v $PWD:/code overcoach 
+docker run -v $PWD:/code overcoach 2> error.log || error_handler()
 eog images/*
