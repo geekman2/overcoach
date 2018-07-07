9e42d7a21bd55993da3d362ba8c67a5f12d2e734
Devon Muraoka <geekmann2@gmail.com>
2018-07-07 15:56:18 -0700

    Use different error handlng

--- overcoach.sh
+++ overcoach.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-function error_handler {
-	original_message=`git log -1 --pretty=%B`
-	error_message=`cat error.log`
-	full_message=`echo $original_message \n\n $error_message`
+error() {
+	local original_message=`git log -1 --pretty=%B`
+	local error_message="$2"
+	local full_message=`echo $original_message \n\n $error_message`
 	git commit --amend -m "$full_message"
 	
 	echo $error_message
@@ -14,5 +14,5 @@ git add .
 git commit -m "$*"
 rm -f images/*
 #docker build -t overcoach .
-docker run -v $PWD:/code overcoach 2> error.log || error_handler()
+trap docker run -v $PWD:/code overcoach 0
 eog images/*
