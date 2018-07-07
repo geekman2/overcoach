ce7c3255939c310d2f1dec48a7d7afc44bb6fea0
Devon Muraoka <geekmann2@gmail.com>
2018-07-07 16:02:17 -0700

    Remove error handling

--- overcoach.sh
+++ overcoach.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-error() {
-	local original_message=`git log -1 --pretty=%B`
-	local error_message="$2"
-	local full_message=`echo $original_message \n\n $error_message`
-	git commit --amend -m "$full_message"
-	
-	echo $error_message
-	exit 1
-}
-
-drun(){
-	docker run -v $PWD:/code overcoach
-}
-
 git add .
 git commit -m "$*"
 rm -f images/*
 #docker build -t overcoach .
-trap drun 0
+docker run -v $PWD:/code overcoach 
 eog images/*
