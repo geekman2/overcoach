10f0c74d05d1b005f788f89f87e498056a42a88e
Devon Muraoka <geekmann2@gmail.com>
2018-07-07 16:27:57 -0700

    Syntax fix

--- image_utils.py
+++ image_utils.py
@@ -10,7 +10,7 @@ class ImageCleaner(object):
 		self.count = 0
 		self.colormap = {'red':[255,0,0,255],
 						 'green':[0,255,0,255],
-						 'blue':[0,0,255,255]
+						 'blue':[0,0,255,255],
 						 'black':[0,0,0,255],
 						 'white':[255,255,255,255]
 					}
